e24f615d55bfdc8780a449257cfb075d5157cde5
摩羯座 <85685966+2289816251@users.noreply.github.com>
2025-06-06 08:17:37 +0800

    Delete .history directory

--- .history/src/api/index_20250124152502.js
+++
@@ -1,5 +0,0 @@
-import home from './ask/home'
-
-export default {
-    ...home
-}
--- .history/src/hooks/useLocalStorageOrders/index_20250529152019.js
+++
@@ -1,7 +0,0 @@
-function setOrders(data){
-    localStorage.setItem('Stars_Orders',data)
-}
-
-function getOrders(data){
-    localStorage.getItem('Stars_Orders')
-}
